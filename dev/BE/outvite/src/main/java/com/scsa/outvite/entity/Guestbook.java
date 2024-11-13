fcfb16884912d6e4278e73d803fa172c9f8506d0
sherry <rlaehdgus95@gmail.com>
2024-11-13 16:10:17 +0900

    :art:_Guestbook_nullable_(#7)

--- dev/BE/outvite/src/main/java/com/scsa/outvite/entity/Guestbook.java
+++ dev/BE/outvite/src/main/java/com/scsa/outvite/entity/Guestbook.java
@@ -4,10 +4,8 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+import org.antlr.v4.runtime.misc.NotNull;
 
 import java.util.Date;
 
@@ -22,18 +20,18 @@ public class Guestbook {
     @Column(name = "id")
     private int id;
 
-    @Column(name = "invitation_id")
+    @Column(name = "invitation_id", nullable = false)
     private String invitationId;
 
-    @Column(name = "name")
+    @Column(name = "name", nullable = false)
     private String name;
 
-    @Column(name = "password")
+    @Column(name = "password", nullable = false)
     private String password;
 
-    @Column(name = "content")
+    @Column(name = "content", nullable = false)
     private String content;
 
-    @Column(name = "created_at")
+    @Column(name = "created_at", nullable = false)
     private Date createdAt;
 }
