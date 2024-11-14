af0f013126698ee2e8a701dca7b0e7d6ac355742
GBGreenBravo <gbgreenbravo@gmail.com>
2024-11-14 16:51:20 +0900

    :art:_add PATCH method_(#29)

--- dev/BE/outvite/src/main/java/com/scsa/outvite/global/config/WebConfig.java
+++ dev/BE/outvite/src/main/java/com/scsa/outvite/global/config/WebConfig.java
@@ -10,6 +10,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("/localhost:3000", "/localhost:3001")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS");
     }
 }
