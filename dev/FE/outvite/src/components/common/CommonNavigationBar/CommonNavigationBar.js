b1d11768be1f1d5991604ccbc1f75ddb9cf27909
donginLee <donginyee@gmail.com>
2024-11-14 10:44:36 +0900

    :sparkles:_공통 네비게이션 바 컴포넌트 구현

--- dev/FE/outvite/src/components/common/CommonNavigationBar/CommonNavigationBar.js
+++ dev/FE/outvite/src/components/common/CommonNavigationBar/CommonNavigationBar.js
@@ -1,1 +1,37 @@
-import react from "react"
+import React from 'react';
+import './CommonNavigationBar.css';
+
+const CommonNavigationBar = ({
+  itemsList,
+  layoutMode = 'space-between',
+  showSeperator = false,
+  itemSpacing = '12px',
+  width = '100%',
+}) => {
+  return (
+    <div
+      className={`navbar-container ${layoutMode === 'padding' ? 'padding-mode' : 'space-mode'}`}
+      style={{
+        width: layoutMode === 'space-between' ? width : 'auto',
+        gap: layoutMode === 'padding' ? itemSpacing : '0',
+      }}
+    >
+      {itemsList.map((item, index) => (
+        <React.Fragment key={index}>
+          <div
+            className="navbar-item"
+            style={{ color: item.color }}
+            onClick={item.onClick}
+          >
+            {item.name}
+          </div>
+          {showSeperator && index < itemsList.length - 1 && (
+            <div className="navbar-separator">|</div>
+          )}
+        </React.Fragment>
+      ))}
+    </div>
+  );
+};
+
+export default CommonNavigationBar;
