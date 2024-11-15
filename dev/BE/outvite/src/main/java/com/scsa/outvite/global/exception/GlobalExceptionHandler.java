a187815d0af6fd0959c7ef2894a44413f49439ad
GBGreenBravo <gbgreenbravo@gmail.com>
2024-11-15 12:51:57 +0900

    :chart_with_upwards_trend:_Error Message for Unhandled Error_(#39)

--- dev/BE/outvite/src/main/java/com/scsa/outvite/global/exception/GlobalExceptionHandler.java
+++ dev/BE/outvite/src/main/java/com/scsa/outvite/global/exception/GlobalExceptionHandler.java
@@ -1,13 +1,13 @@
 package com.scsa.outvite.global.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.Arrays;
-
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
     //비즈니스 로직 예외 처리 -> BadRequest의 역할
     @ExceptionHandler(BusinessException.class)
@@ -28,8 +28,9 @@ public class GlobalExceptionHandler {
     //그 외 모든 예외 처리 -> InternalServerError의 역할
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleException(Exception e) {
+        log.error(e.getMessage());
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), Arrays.stream(e.getStackTrace()).toList().toString()));
+                .body(new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage()));
     }
 }
