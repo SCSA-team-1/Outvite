aba02082922dadd48be18ddb140edab51ccc86e5
sherry <rlaehdgus95@gmail.com>
2024-11-15 10:14:13 +0900

    :art:_adjust_AuthController

--- dev/BE/outvite/src/main/java/com/scsa/outvite/auth/controller/AuthController.java
+++ dev/BE/outvite/src/main/java/com/scsa/outvite/auth/controller/AuthController.java
@@ -1,18 +1,38 @@
 package com.scsa.outvite.auth.controller;
 
-import com.scsa.outvite.auth.dto.CheckDuplicatedIdResponse;
-import com.scsa.outvite.auth.dto.SignupResponse;
+import com.scsa.outvite.auth.dto.*;
 import com.scsa.outvite.global.ApiResponse;
-import com.scsa.outvite.auth.dto.SignupRequest;
 import com.scsa.outvite.auth.service.AuthService;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import static com.scsa.outvite.global.ApiConstants.MEMBER_ID;
+
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/auth/")
+@RequestMapping("/api/auth")
 public class AuthController {
     private final AuthService authService;
+
+    @PostMapping("/login")
+    public ApiResponse login(@RequestBody LoginRequest request, HttpSession session) {
+        session.removeAttribute(MEMBER_ID);
+        LoginResponse response = authService.login(request.getId(), request.getPassword());
+        session.setAttribute(MEMBER_ID, request.getId());
+        return ApiResponse.builder()
+                .message("로그인 되었습니다.")
+                .data(response)
+                .build();
+    }
+
+    @GetMapping("/logout")
+    public ApiResponse logout(HttpSession session){
+        session.invalidate();
+        return ApiResponse.builder()
+                .message("로그아웃 되었습니다")
+                .build();
+    }
 
     @PostMapping("/signup")
     public ApiResponse signup(@RequestBody SignupRequest request) {
