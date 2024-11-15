ab2d3a9013980400b7868627efcb5e8e361dae95
GBGreenBravo <gbgreenbravo@gmail.com>
2024-11-15 11:35:26 +0900

    :sparkles:_Guestbook 생성 API_(#36)

---
+++ dev/BE/outvite/src/main/java/com/scsa/outvite/guestbook/error/GuestBookError.java
@@ -0,0 +1,16 @@
+package com.scsa.outvite.guestbook.error;
+
+import com.scsa.outvite.global.exception.ErrorCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum GuestBookError implements ErrorCode {
+    NOT_FOUND_GUESTBOOK(HttpStatus.NOT_FOUND.value(), "존재하지 않는 방명록입니다."),
+    CREATE_GUESTBOOK_FORBIDDEN(HttpStatus.FORBIDDEN.value(), "방명록 작성 권한이 없습니다. 참석 의사 제출 후 이용해 주세요.");
+
+    private final int status;
+    private final String message;
+}
